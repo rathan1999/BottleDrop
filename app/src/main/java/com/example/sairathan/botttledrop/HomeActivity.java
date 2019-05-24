106e945d9d8f8a17ccd530e1015cc776514642b6
Sai Rathan <sairathan.v16@iiits.in>
2019-05-24 19:40:46 +0530

    Firebase Login

--- app/src/main/java/com/example/sairathan/botttledrop/HomeActivity.java
+++ app/src/main/java/com/example/sairathan/botttledrop/HomeActivity.java
@@ -1,10 +1,13 @@
 package com.example.sairathan.botttledrop;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -16,12 +19,20 @@ public class HomeActivity extends AppCompatActivity {
     private ImageView imageView;
     int currentPage = 0;
     Timer timer;
+    TextView textView;
     final long DELAY_MS = 500;
     final long PERIOD_MS = 1500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        textView = (TextView)findViewById(R.id.logintext);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
 
         mSlideViewPager = (ViewPager)findViewById(R.id.slideViewPager);
 
@@ -78,4 +89,8 @@ public class HomeActivity extends AppCompatActivity {
 
         }
     };
+    public void login(){
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
 }
