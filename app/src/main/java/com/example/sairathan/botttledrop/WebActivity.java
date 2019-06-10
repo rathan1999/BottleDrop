a9691f6c377d15de807a1acb075cb77427085696
Sai Rathan <sairathan.v16@iiits.in>
2019-06-10 21:58:51 +0530

    Change Address Screen

--- app/src/main/java/com/example/sairathan/botttledrop/WebActivity.java
+++ app/src/main/java/com/example/sairathan/botttledrop/WebActivity.java
@@ -1,13 +1,64 @@
 package com.example.sairathan.botttledrop;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class WebActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+public class WebActivity extends AppCompatActivity{
+
+    private TextView t,addressArea;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
+    private Button button,sendDetails;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
+        t = findViewById(R.id.addresspane);
+        addressArea=findViewById(R.id.EditAddressPane);
+        button = findViewById(R.id.UpdateAddress);
+        sendDetails=findViewById(R.id.sendDetailsButton);
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        final DatabaseReference address= databaseReference.child("User").child(user.getUid()).child("position");
+        address.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                t.setText(dataSnapshot.getValue(String.class));
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                address.setValue(addressArea.getText().toString().trim());
+            }
+        });
+        sendDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+    private void login(){
+        Intent intent = new Intent(this,Services.class);
+        startActivity(intent);
     }
 }
